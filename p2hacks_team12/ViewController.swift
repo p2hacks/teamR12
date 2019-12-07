001a65cb9d97e081aa2b402c56d70f4ba5b33ade
上野隆斗 <>
2019-12-07 13:45:14 +0900

    [feat]プロジェクトの作成

---
+++ p2hacks_team12/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  p2hacks_team12
+//
+//  Created by 上野隆斗 on 2019/12/07.
+//  Copyright © 2019 anonykous. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
