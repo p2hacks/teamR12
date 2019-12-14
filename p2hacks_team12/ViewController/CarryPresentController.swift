69d91e05e5c66af112bed0cddcab9e5cd49474d5
宮下翔伍 <syogo0323sekaowa@docomo.ne.jp>
2019-12-14 13:58:51 +0900

    子供のトップ画面に音センサーと加速度センサーを検出し、値に応じてデータを変更して送る

--- p2hacks_team12/ViewController/CarryPresentController.swift
+++ p2hacks_team12/ViewController/CarryPresentController.swift
@@ -10,25 +10,23 @@ import UIKit
 class CarryPresentController: UIViewController {
     
     @IBOutlet weak var ShowValue: UILabel! //音の大きさによってメッセージを送る
-    
     @IBOutlet weak var level1View: UIImageView! //音の大きさに応じて画像を表示
     @IBOutlet weak var level2View: UIImageView!
     @IBOutlet weak var level3View: UIImageView!
-    var recorder:ParentAudioRecorder!
+    var recorder: SoundAudioRecorder!
     var timer: Timer!
     var checksound:Float = 0.0 //音の大きさを格納
-    var levelboader1to2:Float = 0.02 //1から2に警告が変わるライン
-    var levelboader2to3:Float = 0.04 //2から3に警告が変わるライン
+    var levelboader1to2:Float = 0.2 //1から2に警告が変わるライン
+    var levelboader2to3:Float = 0.3 //2から3に警告が変わるライン
     var counttimer = 0.0 //一定時間をカウント
     var checktest = 1 //警告の切り替え信号
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        recorder = ParentAudioRecorder()
+        recorder = SoundAudioRecorder()
         recorder?.start()
-        
     }
+    
     //初期に一度だけ実行
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -46,18 +44,16 @@ class CarryPresentController: UIViewController {
     }
     
     @objc func update(tm: Timer){
-        
         checksound = recorder.level
-        print(counttimer)
         counttimer += 1/180 //現実時間に合うように調整
         if(checksound > levelboader1to2 || checktest == 2){ //音の大きさによって警告を変える
             checktest = 2
-            if(counttimer < 2){
+            if(counttimer < 2){ //一定時間警告
                 if(checksound > levelboader2to3){
                     level2View.isHidden = false
                     level3View.isHidden = true
                     checktest = 3
-                    ShowValue.text = "安心！"
+                    ShowValue.text = "危険！"
                     counttimer = 0
                 }
                 level2View.isHidden = false
@@ -70,11 +66,10 @@ class CarryPresentController: UIViewController {
                 ShowValue.text = "安心！"
                 counttimer = 0
             }
-            
         }else if(checksound > levelboader2to3 || checktest == 3){
+             print("value=\(checksound)")
             checktest = 3
             if(counttimer < 4){
-                
                 counttimer += 1/180
                 level2View.isHidden = false
                 level3View.isHidden = false
@@ -87,7 +82,6 @@ class CarryPresentController: UIViewController {
                 counttimer = 0
             }
         }else { counttimer = 0}
-        
     }
     
     /*
--- p2hacks_team12/ViewController/ChildViewController.swift
+++ p2hacks_team12/ViewController/ChildViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ChildViewController: UIViewController {
     
@@ -14,39 +15,88 @@ class ChildViewController: UIViewController {
     @IBOutlet weak var letter: UILabel!
     @IBOutlet weak var Xmas: UILabel!
     let date = DateManager()
+    let motionmanager = CMMotionManager()
     var timer: Timer!
-    
+    var datatimer: Timer!
+    var soundrecoder: SoundAudioRecorder!
+    var acceleration: AccelerationSensor!
+    var checksound: Float = 0.0
+    var sounddata = 1
+    var acceledata = 1
+    var accelX: Double = 0.0
+    var accelY: Double = 0.0
+    var accelZ: Double = 0.0
+    var counttime = 0
     override func viewDidLoad() {
         super.viewDidLoad()
+        if motionmanager.isAccelerometerAvailable {
+            // intervalの設定 [sec]
+            motionmanager.accelerometerUpdateInterval = 1.0
+            // センサー値の取得開始
+            motionmanager.startAccelerometerUpdates(
+                to: OperationQueue.current!,
+                withHandler: {(accelData: CMAccelerometerData?, errorOC: Error?) in
+                    self.InputAccelerationdata(acceleration: accelData!.acceleration)
+            })
+        }
+        soundrecoder = SoundAudioRecorder()
+        acceleration = AccelerationSensor()
+        soundrecoder?.start()
         // Do any additional setup after loading the view.
         Xmas.text = "クリスマスまであと"
         letter.text = "サンタさんへ手紙を書こう!"
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool) { //一度だけ処理を実行
         super.viewWillAppear(true)
         //カウントダウン
-        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+        datatimer =
+            Timer.scheduledTimer(timeInterval: 0.01, target: self, selector: #selector(self.InputSoundData), userInfo: nil, repeats: true)
+        datatimer.fire()
         timer.fire()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(true)
+        super.viewWillDisappear(animated)
         timer.invalidate()
-        timer = nil
     }
     
-    @objc func update(tm: Timer) {
-
+    func InputAccelerationdata(acceleration: CMAcceleration){
+        accelX = acceleration.x
+        accelY = acceleration.y
+        if((accelX < 0.1 && accelX > -0.1)&&(accelY < 0.1 && accelY > -0.1)){
+            counttime += 1
+            print(counttime)
+            if(counttime < 5){
+                acceledata = 2
+                print("加速度安心度はレベル2です")
+            }else{
+                print("加速度安心度はレベル3です")
+                acceledata = 3
+            }
+        }else{
+            counttime = 0
+            print("加速度安心度はレベル1です")
+            acceledata = 1
+        }
+    }
+    @objc func InputSoundData(tm: Timer){ //子供端末で取得したセンサーの値を格納
+        checksound = soundrecoder.level
+        if(checksound > 0.10){
+            sounddata = 3
+        }else if(checksound > 0.03){
+            sounddata = 2
+        }else{
+            sounddata = 1
+        }
+        // print(sounddata)
+        
+    }
+    @objc func update(tm: Timer) { //現在の日付を取得
         let count: Int = date.getXmaxTimeInterval()
         let count2: TimeInterval = TimeInterval(count)
         let formatter = DateComponentsFormatter()
-
         // 表示フォーマットを変更．.positionalや.fullで表示が変わります．
         formatter.unitsStyle = .brief
         // 使用する単位　.minuteのみにすると232,071minのように出力されます．
@@ -62,15 +112,15 @@ class ChildViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
