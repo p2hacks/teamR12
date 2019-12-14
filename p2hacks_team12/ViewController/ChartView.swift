0165db943002a23ced58433219ba2e4ae5632e7e
池田恵徒 <b1017065@fun.ac.jp>
2019-12-14 16:07:59 +0900

    [fix]グラフ追加

--- p2hacks_team12/ViewController/ChartView.swift
+++ p2hacks_team12/ViewController/ChartView.swift
@@ -24,6 +24,19 @@ class ChartView: UIView {
         caBasicAnimation.toValue = 1.0
         caShapeLayerForValue.add(caBasicAnimation, forKey: "chartAnimation")
     }
+    
+    func drawChartMini(rate:Double){
+        //グラフを表示
+        drawBaseChartMini()
+        drawValueChartMini(rate: rate)
+
+        //グラフをアニメーションで表示
+        let caBasicAnimation = CABasicAnimation(keyPath: "strokeEnd")
+        caBasicAnimation.duration = 2.0
+        caBasicAnimation.fromValue = 0.0
+        caBasicAnimation.toValue = 1.0
+        caShapeLayerForValue.add(caBasicAnimation, forKey: "chartAnimation")
+    }
 
     /**
      円グラフの軸となる円を表示
@@ -34,6 +47,21 @@ class ChartView: UIView {
         caShapeLayerForBase.strokeColor = UIColor(displayP3Red: 1, green: 0.8, blue: 0.4, alpha: 1.0).cgColor
         caShapeLayerForBase.fillColor = UIColor.clear.cgColor
         caShapeLayerForBase.lineWidth = 50
+        caShapeLayerForBase.lineCap = .round
+
+        let startAngle:CGFloat = CGFloat(0.0)
+        let endAngle:CGFloat = CGFloat(Double.pi * 2.0)
+
+        caShapeLayerForBase.path = UIBezierPath.init(arcCenter: CGPoint.init(x: shapeFrame.size.width / 2.0, y: shapeFrame.size.height / 2.0),radius: shapeFrame.size.width / 2.0,startAngle: startAngle,endAngle: endAngle,clockwise: true).cgPath
+        self.layer.addSublayer(caShapeLayerForBase)
+    }
+    
+    private func drawBaseChartMini(){
+        let shapeFrame = CGRect.init(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        caShapeLayerForBase.frame = shapeFrame
+        caShapeLayerForBase.strokeColor = UIColor(displayP3Red: 1, green: 0.8, blue: 0.4, alpha: 1.0).cgColor
+        caShapeLayerForBase.fillColor = UIColor.clear.cgColor
+        caShapeLayerForBase.lineWidth = 20
         caShapeLayerForBase.lineCap = .round
 
         let startAngle:CGFloat = CGFloat(0.0)
@@ -56,6 +84,28 @@ class ChartView: UIView {
         caShapeLayerForValue.strokeColor = UIColor(displayP3Red: 1, green: 0.4, blue: 0.4, alpha: 1).cgColor
         caShapeLayerForValue.fillColor = UIColor.clear.cgColor
         caShapeLayerForValue.lineWidth = 50
+        caShapeLayerForValue.lineCap = .round
+
+        //開始位置を時計の0時の位置にする
+        let startAngle:CGFloat = CGFloat(-1 * Double.pi / 2.0)
+
+        //終了位置を時計の0時起点で引数渡しされた割合の位置にする
+        let endAngle :CGFloat = CGFloat(rate / 100 * Double.pi * 2.0 - (Double.pi / 2.0))
+
+        //UIBezierPathを使用して半円を定義
+        caShapeLayerForValue.path = UIBezierPath.init(arcCenter: CGPoint.init(x: shapeFrame.size.width / 2.0, y: shapeFrame.size.height / 2.0),radius: shapeFrame.size.width / 2.0,startAngle: startAngle,endAngle: endAngle,clockwise: true).cgPath
+        self.layer.addSublayer(caShapeLayerForValue)
+    }
+    
+    private func drawValueChartMini(rate:Double){
+        //CAShareLayerを描く大きさを定義
+        let shapeFrame = CGRect.init(x: 0, y: 0, width: self.frame.width, height: self.frame.height)
+        caShapeLayerForValue.frame = shapeFrame
+
+        //CAShareLayerのデザインを定義
+        caShapeLayerForValue.strokeColor = UIColor(displayP3Red: 1, green: 0.4, blue: 0.4, alpha: 1).cgColor
+        caShapeLayerForValue.fillColor = UIColor.clear.cgColor
+        caShapeLayerForValue.lineWidth = 20
         caShapeLayerForValue.lineCap = .round
 
         //開始位置を時計の0時の位置にする
--- p2hacks_team12/ViewController/ParentViewController.swift
+++ p2hacks_team12/ViewController/ParentViewController.swift
@@ -13,6 +13,24 @@ class ParentViewController: UIViewController {
     private let labelRate:UILabel = UILabel()
     private let buttonDraw:UIButton = UIButton()
     private let chartView:ChartView = ChartView()
+    private let textRateOne:UITextView = UITextView()
+    private let labelRateOne:UILabel = UILabel()
+    private let buttonDrawOne:UIButton = UIButton()
+    private let chartViewOne:ChartView = ChartView()
+    private let textRateTwo:UITextView = UITextView()
+    private let labelRateTwo:UILabel = UILabel()
+    private let buttonDrawTwo:UIButton = UIButton()
+    private let chartViewTwo:ChartView = ChartView()
+    private let textRateThree:UITextView = UITextView()
+    private let labelRateThree:UILabel = UILabel()
+    private let buttonDrawThree:UIButton = UIButton()
+    private let chartViewThree:ChartView = ChartView()
+    
+    @IBOutlet weak var dangerRate: UILabel!
+    @IBOutlet weak var degreeOfRisk: UILabel!
+    
+    
+    
     
     
     override func viewDidLoad() {
@@ -37,23 +55,84 @@ class ParentViewController: UIViewController {
         
         changeScreen()
         
+        textRateOne.layer.cornerRadius = 10
+        textRateOne.layer.borderColor = UIColor.lightGray.cgColor
+        textRateOne.layer.borderWidth = 0.5
+        textRateOne.keyboardType = .numberPad
+        textRateOne.text = "30" //とりあえずデフォル値は30%
+        textRateOne.font = UIFont.systemFont(ofSize: 16)
+        self.view.addSubview(textRateOne)
+        
+        labelRateOne.text = "%"
+        self.view.addSubview(labelRateOne)
+        buttonDrawOne.setTitle("グラフ表示", for: .normal)
+        buttonDrawOne.setTitleColor(UIColor.blue, for: .normal)
+        buttonDrawOne.addTarget(self, action: #selector(self.touchUpButtonDrawOne), for: .touchUpInside)
+        self.view.addSubview(buttonDrawOne)
+        
+        self.view.addSubview(chartViewOne)
+        
+        changeScreenOne()
+        
+        textRateTwo.layer.cornerRadius = 10
+        textRateTwo.layer.borderColor = UIColor.lightGray.cgColor
+        textRateTwo.layer.borderWidth = 0.5
+        textRateTwo.keyboardType = .numberPad
+        textRateTwo.text = "50" //とりあえずデフォル値は50%
+        textRateTwo.font = UIFont.systemFont(ofSize: 16)
+        self.view.addSubview(textRateTwo)
+        
+        labelRateTwo.text = "%"
+        self.view.addSubview(labelRateTwo)
+        buttonDrawTwo.setTitle("グラフ表示", for: .normal)
+        buttonDrawTwo.setTitleColor(UIColor.blue, for: .normal)
+        buttonDrawTwo.addTarget(self, action: #selector(self.touchUpButtonDrawTwo), for: .touchUpInside)
+        self.view.addSubview(buttonDrawTwo)
+        
+        self.view.addSubview(chartViewTwo)
+        
+        changeScreenTwo()
+        
+        textRateThree.layer.cornerRadius = 10
+        textRateThree.layer.borderColor = UIColor.lightGray.cgColor
+        textRateThree.layer.borderWidth = 0.5
+        textRateThree.keyboardType = .numberPad
+        textRateThree.text = "10" //とりあえずデフォル値は10%
+        textRateThree.font = UIFont.systemFont(ofSize: 16)
+        self.view.addSubview(textRateThree)
+        
+        labelRateThree.text = "%"
+        self.view.addSubview(labelRateThree)
+        buttonDrawThree.setTitle("グラフ表示", for: .normal)
+        buttonDrawThree.setTitleColor(UIColor.blue, for: .normal)
+        buttonDrawThree.addTarget(self, action: #selector(self.touchUpButtonDrawThree), for: .touchUpInside)
+        self.view.addSubview(buttonDrawThree)
+        
+        self.view.addSubview(chartViewThree)
+        
+        changeScreenThree()
+        
     }
     
     /**
      端末の向きの変更のイベント
      */
-    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-        coordinator.animate(
-            alongsideTransition: nil,
-            completion: {(UIViewControllerTransitionCoordinatorContext) in
-                self.changeScreen()
-        }
-        )
-    }
+    /*override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+     super.viewWillTransition(to: size, with: coordinator)
+     coordinator.animate(
+     alongsideTransition: nil,
+     completion: {(UIViewControllerTransitionCoordinatorContext) in
+     self.changeScreen()
+     }
+     )
+     }*/
     
     override func viewWillAppear(_ animated: Bool) {
         drawChart()
+        drawChartOne()
+        drawChartTwo()
+        drawChartThree()
+        
     }
     
     private func changeScreen(){
@@ -61,9 +140,12 @@ class ParentViewController: UIViewController {
         let widthValue = screenSize.width
         let heightValue = screenSize.height
         
-        textRate.frame = CGRect(x: widthValue/2-170, y: 200, width: 100, height: 40)
-        labelRate.frame = CGRect(x: widthValue/2-70, y: 200, width: 40, height: 40)
-        buttonDraw.frame = CGRect(x: widthValue/2-30, y: 200, width: 200, height: 40)
+        
+        textRate.frame = CGRect(x: widthValue/2-170, y: 10, width: 100, height: 40)
+        labelRate.frame = CGRect(x: widthValue/2-70, y: 10, width: 40, height: 40)
+        buttonDraw.frame = CGRect(x: widthValue/2-30, y: 10, width: 200, height: 40)
+        
+        
         
         var drawWidth = widthValue * 0.8
         if (widthValue > heightValue){
@@ -74,8 +156,79 @@ class ParentViewController: UIViewController {
         
     }
     
+    private func changeScreenOne(){
+        let screenSize: CGRect = UIScreen.main.bounds
+        let widthValue = screenSize.width
+        let heightValue = screenSize.height
+        
+        textRateOne.frame = CGRect(x: widthValue/2-170, y: 50, width: 100, height: 40)
+        labelRateOne.frame = CGRect(x: widthValue/2-70, y: 50, width: 40, height: 40)
+        buttonDrawOne.frame = CGRect(x: widthValue/2-30, y: 50, width: 200, height: 40)
+        
+        var drawWidth = widthValue * 0.8
+        if (widthValue > heightValue){
+            drawWidth = heightValue * 0.8
+        }
+        chartViewOne.frame = CGRect(x: 20, y: 570, width: 80, height: 80)
+        
+        
+    }
+    
+    private func changeScreenTwo(){
+        let screenSize: CGRect = UIScreen.main.bounds
+        let widthValue = screenSize.width
+        let heightValue = screenSize.height
+        
+        textRateTwo.frame = CGRect(x: widthValue/2-170, y: 90, width: 100, height: 40)
+        labelRateTwo.frame = CGRect(x: widthValue/2-70, y: 90, width: 40, height: 40)
+        buttonDrawTwo.frame = CGRect(x: widthValue/2-30, y: 90, width: 200, height: 40)
+        
+        var drawWidth = widthValue * 0.8
+        if (widthValue > heightValue){
+            drawWidth = heightValue * 0.8
+        }
+        chartViewTwo.frame = CGRect(x: 150, y: 570, width: 80, height: 80)
+        
+        
+    }
+    
+    private func changeScreenThree(){
+        let screenSize: CGRect = UIScreen.main.bounds
+        let widthValue = screenSize.width
+        let heightValue = screenSize.height
+        
+        textRateThree.frame = CGRect(x: widthValue/2-170, y: 90, width: 100, height: 40)
+        labelRateThree.frame = CGRect(x: widthValue/2-70, y: 90, width: 40, height: 40)
+        buttonDrawThree.frame = CGRect(x: widthValue/2-30, y: 90, width: 200, height: 40)
+        
+        var drawWidth = widthValue * 0.8
+        if (widthValue > heightValue){
+            drawWidth = heightValue * 0.8
+        }
+        chartViewThree.frame = CGRect(x: 280, y: 570, width: 80, height: 80)
+        
+        
+    }
+    
+    
     @objc func touchUpButtonDraw(){
         drawChart()
+        
+    }
+    
+    @objc func touchUpButtonDrawOne(){
+        drawChartOne()
+        
+    }
+    
+    @objc func touchUpButtonDrawTwo(){
+        drawChartTwo()
+        
+    }
+    
+    @objc func touchUpButtonDrawThree(){
+        drawChartThree()
+        
     }
     
     /**
@@ -85,6 +238,23 @@ class ParentViewController: UIViewController {
         let rate = Double(textRate.text!)
         chartView.drawChart(rate: rate!)
     }
+    
+    private func drawChartOne(){
+        let rate = Double(textRateOne.text!)
+        chartViewOne.drawChartMini(rate: rate!)
+    }
+    
+    private func drawChartTwo(){
+        let rate = Double(textRateTwo.text!)
+        chartViewTwo.drawChartMini(rate: rate!)
+    }
+    
+    private func drawChartThree(){
+        let rate = Double(textRateThree.text!)
+        chartViewThree.drawChartMini(rate: rate!)
+    }
+    
+    
     
     /*
      // MARK: - Navigation
